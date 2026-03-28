026e4817b3821b65da510fff9eff12545bd22f33
Michi <Michi>
2026-03-28 19:14:36 +0100

    fix: resolve typescript error with spread argument in WebGLRenderer

--- src/components/Plot/WebGLRenderer.tsx
+++ src/components/Plot/WebGLRenderer.tsx
@@ -281,7 +281,7 @@ function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
   return shader;
 }
 
-function hexToRgba(hex: string): number[] {
+function hexToRgba(hex: string): [number, number, number] {
   const r = parseInt(hex.slice(1, 3), 16) / 255;
   const g = parseInt(hex.slice(3, 5), 16) / 255;
   const b = parseInt(hex.slice(5, 7), 16) / 255;
